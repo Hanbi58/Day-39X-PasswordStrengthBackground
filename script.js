5a87de05e0aa502bf35f757456e739eaf0a37e6a
Hanbi58 <hanxiaonan2012@gmail.com>
2022-10-09 18:39:41 -0700

    My version done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert('1')
+const pswIpt = document.getElementById("password");
+const bgImg = document.querySelector(".background");
+pswIpt.addEventListener("input", function () {
+  updateBg(this.value.trim());
+});
+
+function updateBg(str) {
+  let pxnum = 100;
+  const arr = str.split("").filter((item) => item != " ");
+  arr.length > 7 && (pxnum -= 25);
+  arr.some((cha) => isNaN(+cha) && cha === cha.toUpperCase()) && (pxnum -= 25);
+  arr.some((cha) => isNaN(+cha) && cha === cha.toLowerCase()) && (pxnum -= 25);
+  arr.some((cha) => !isNaN(+cha)) && (pxnum -= 25);
+  bgImg.style.filter = `blur(${pxnum}px)`;
+  console.log(pxnum);
+}
